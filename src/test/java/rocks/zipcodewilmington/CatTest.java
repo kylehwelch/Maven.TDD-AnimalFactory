2c23915008fbf6b6a4bf831d8e8e453dba27be05
Kyle Welch <welch@udel.edu>
2022-07-10 13:57:58 -0400

    catTest done.

--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -2,7 +2,11 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Mammal;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
 
 import java.util.Date;
 
@@ -10,13 +14,22 @@ import java.util.Date;
  * @author leon on 4/19/18.
  */
 public class CatTest {
-    // TODO - Create tests for `void setName(String name)`
-    // TODO - Create tests for `speak`
-    // TODO - Create tests for `setBirthDate(Date birthDate)`
-    // TODO - Create tests for `void eat(Food food)`
-    // TODO - Create tests for `Integer getId()`
-    // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
-    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
+    // DONE - Create tests for `void setName(String name)`
+    // DONE - Create tests for `speak`
+    // DONE - Create tests for `setBirthDate(Date birthDate)`
+    // DONE - Create tests for `void eat(Food food)`
+    // DONE - Create tests for `Integer getId()`
+    // DONE - Create test to check Animal inheritance; google search `java instanceof keyword`
+    // DONE - Create test to check Mammal inheritance; google search `java instanceof keyword`
+
+    //Given
+
+
+    //When
+
+
+    //Then
+
 
 
     @Test
@@ -40,4 +53,93 @@ public class CatTest {
         Assert.assertEquals(givenId, retrievedId);
     }
 
+    @Test
+    public void testSetName(){
+        //Given
+        String name = "Pickle";
+        Date catBDay = new Date("2015/10/21");
+        int id = 01;
+        Cat cat = new Cat(name, catBDay, id);
+
+        //When
+        cat.setName("Pickle");
+        String expected = "Pickle";
+
+        //Then
+        String actual = cat.getName();
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testCatspeak(){
+        //Given
+        Date date = new Date("2015/10/21");
+        Cat cat = new Cat("a", date, 01);
+
+        //When
+        String expected = "meow!";
+        String actual = cat.speak();
+
+        //Then
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void testCatBirthday(){
+        //Given
+        Date date = new Date("2015/10/21");
+        Cat cat = new Cat("a", date, 01);
+
+        //When
+        Date expected = date;
+        Date actual = cat.getBirthDate();
+
+        //Then
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void catEatTest(){
+        //Given
+        Date date = new Date("2015/10/21");
+        Cat cat = new Cat("a", date, 01);
+        Food chicken = new Food();
+        //When
+        cat.eat(chicken);
+        int mealAct = cat.getNumberOfMealsEaten();
+        int  mealExp = 1;
+        //Then
+        Assert.assertEquals(mealAct, mealExp);
+    }
+    @Test
+    public void catGetID(){
+        //Given
+        Date date = new Date("2015/10/21");
+        Cat cat = new Cat("a", date, 01);
+        //When
+        int expected = 1;
+        int actual = cat.getId();
+        //Then
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void catTestAnimalInheritance(){
+        //Given
+        Date date = new Date("2015/10/21");
+        Cat cat = new Cat("a", date, 01);
+        //When
+        boolean actual = cat instanceof Animal;
+        boolean expected = true;
+        //Then
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void catTestMammalInheritance(){
+        //Given
+        Date date = new Date("2015/10/21");
+        Cat cat = new Cat("a", date, 01);
+        //When
+        boolean actual = cat instanceof Mammal;
+        boolean expected = true;
+        //Then
+        Assert.assertEquals(actual, expected);
+    }
 }
